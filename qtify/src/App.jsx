9ee93265f8d480878f69041fd3d53c06c47bae85
nalina <naleenameshram85@gmail.com>
2024-06-16 19:50:15 +0530

    qtifytwo

--- qtify/src/App.jsx
+++ qtify/src/App.jsx
@@ -1,8 +1,10 @@
-import Navbars from "./component/Navbars/Navbar";
+import HeroImage from "./component/HeroImage/HeroImage";
+ import Navbars from "./component/Navbars/Navbar";
 export default function App() {
   return (
     <div className="App">
-      <Navbars/>
+       <Navbars/> 
+      <HeroImage/>
     </div>
   )
 }
---
+++ qtify/src/component/HeroImage/HeroImage.jsx
@@ -0,0 +1,16 @@
+import Box from '@mui/material/Box';
+ import styles from '../HeroImage/HeroImage.module.css';
+const HeroImage = () =>{
+ return (
+    <div className={styles.HeroImage}>
+        <Box className={styles.Container}>
+        <img className={styles.imgSrc} src="./src/assets/hero_headphones.png" alt="iamge"/>
+        <Box className={styles.TextContainer}>
+          <h2 className={styles.Text1}>100 Thousand Songs,ad-free</h2>
+          <h2 className={styles.Text}>Over thousands podcast episcode</h2>
+        </Box>
+        </Box>
+    </div>
+ )
+}
+export default HeroImage;
